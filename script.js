bcc74ca2cffb3540f611dc267196357dc97f14d4
Nnamdi <nnamdidanielosuji@gmail.com>
2022-01-06 22:00:08 +0100

    date and time

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const currencyEL_one = document.getElementById('CurrencyOne')
 const currencyEL_two = document.getElementById('CurrencyTwo')
 const fromAmount = document.getElementById('amount-one')
 const toAmount = document.getElementById('convertedAmount')
+const dateEl = document.getElementById('date')
 
 const rateEl = document.getElementById('rate')
 const swapEl = document.getElementById('swap')
@@ -40,3 +41,24 @@ swapEl.addEventListener('click', ()=>{
 
 
 calculate()
+
+
+// get date and time
+
+var days =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var months =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+
+var date = new Date()
+var month = date.getMonth()
+var curDate = date.getDate()
+var day = date.getDay()
+var year = date.getFullYear()
+console.log(year)
+console.log(day)
+console.log(month)
+const completeDate = days[day] + ', ' + curDate + ' ' + months[month] + ' ' + year
+console.log(curDate)
+
+
+
+dateEl.innerText = completeDate
